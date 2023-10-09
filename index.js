43df5d23e25a5c1db954b4014f041a8dfb7c6854
Ryan Huellen <ryan@huellenconsulting.com>
2023-10-09 01:24:07 -0500

    fix: relative paths

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ const currentPath = window.location.pathname
               </div>
             </div>
             <a
-                href="/service.html?id=${id}"
+                href="./service.html?id=${id}"
                 class="mt-4 rounded-md bg-red-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-red-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-red-600"
                 >Learn more</a
               >
